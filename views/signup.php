55312c1ea2a7da8ff4f95c2b8e03fa3732896926
IchiharaGaku <gi226bb@ezweb.ne.jp>
2020-09-14 00:07:28 +0900

    login-css

--- views/signup.php
+++ views/signup.php
@@ -1,13 +1,12 @@
 <?php require('./components/header.php') ?>
 
 <body>
+    <header>
+        <h1 class="user_form_header">TimeSchedule</h1>
+    </header>
     <div class="user_form">
-
-        <header>
-            <h1 class="user_form_header">TimeSchedule</h1>
-        </header>
-
         <form action="./" method="">
+        <div class="form_section">
             <h2 class="form_title">signup</h2>
 
             <h3 class="username_title">Username</h3>
@@ -15,11 +14,11 @@
             <h3 class="email_title">Password</h3>
             <input type="text" class="user_password">
 
-            <input type="submit" value="Sign up">
+            <input type="submit" value="Sign up" class="submit">
 
-            <a href="#">Log in</a>
+            <a href="#" class="login">Log in</a>
+        </div>
         </form>
-
     </div>
     <script src="../main.js"></script>
 
